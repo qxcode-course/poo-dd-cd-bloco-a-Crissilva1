bede0bf030ef67acf4b7897b6ed79d935fea78e0
Crissilva1 <cristianedasilva1267@gmail.com>
2025-09-19 13:59:29 -0300

    alteração dados toalha

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,14 +1,14 @@
 class Towel:
     def __init__(self):
-        self.color ="pink"
-        self.size = "m"
+        self.color ="black"
+        self.size = "p"
         self.wetness =0
     def __str__(self):
         return f"color:{self.color}, tam:{self.size}, hum:{self.wetness}"    
 
 towel= Towel()
 print(towel.color)
-towel.color = "red"
+towel.color = "yellow"
 print(towel.color)
 print(towel.size)
 print(towel.wetness)
