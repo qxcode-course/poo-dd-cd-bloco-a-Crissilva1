eaed7ae2cf627717e25c9d5171448df2f9ef8400
Crissilva1 <cristianedasilva1267@gmail.com>
2025-10-04 19:27:24 +0000

    atividade carro concluida com ajuda

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,5 +1,5 @@
-class drive:
-    def __init__(self):
+class Carro:
+    def __init__(self, pass3 : int , km:int , gas:int):
        self.pass3: int = 0
        self.km: int = 0
        self.passMax: int = 2
@@ -7,14 +7,62 @@ class drive:
        self.gasMax : int = 100
 
     def __str__ (self) -> str:
-           return (f"passagem:{self.pass3}, gas:{self.gas}, kilometro:{self.km}")
+           return (f"pass: {self.pass3}, gas: {self.gas}, km: {self.km}")
        
-    def enter (self, enter:int)-> None :
-        self.passMax +=1
-        if self.passMax > 2:
-           self.passMax = 2
-           return 
+    def enter (self)-> None :
+        if self.pass3 < self.passMax:
+            self.pass3 += 1
+        else:
+            print("fail: limite de pessoas atingido")    
+           
         
-    def leave (self,leave :int)-> None:
-        self.passMax -=2
-        if self.pass
+    def leave(self) -> None:
+      if self.pass3 <= 0:
+        print("fail: nao ha ninguem no carro")
+        return
+      self.pass3 -= 1
+
+         
+    def fuel (self,increment:int):     
+        self.gas += increment
+        if self.gas > 100 :
+            self.gas = 100
+
+    def drive (self,distance: int)-> None:
+         if self.pass3 == 0:
+            print ("fail: nao ha ninguem no carro")
+            return
+         if self.gas == 0:
+            print ("fail: tanque vazio")
+            return
+         if distance<= self.gas :
+             self.km += distance
+             self.gas -= distance
+         else:
+             kmandado =self.gas
+             self.km += kmandado
+             self.gas=0
+             print (f"fail: tanque vazio apos andar {kmandado} km")
+
+def main():
+    carro=Carro (" " , " " , " ") 
+    while True: 
+        line : str = input()     
+        print ("$" + line)  
+        args : list [str] = line.split (" ")   
+        if args [0]=="end":
+            break
+        if args [0]== "show":
+            print (carro)
+        if args [0]== "enter":
+            carro.enter()
+        if args [0] == "leave":
+            carro.leave()
+        if args[0] == "fuel":
+         increment = int(args[1]) 
+         carro.fuel(increment)
+        if args [0] == "drive" :
+            distance = int (args[1]) 
+            carro.drive (distance)
+
+main()            
