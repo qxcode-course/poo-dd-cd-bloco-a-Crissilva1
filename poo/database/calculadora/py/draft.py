29fa0dda67d817bea69d542723f6ad7413ea451f
Crissilva1 <cristianedasilva1267@gmail.com>
2025-10-05 21:15:46 +0000

    atividade carro concluida refazendo com ajuda

--- poo/database/calculadora/py/draft.py
+++ poo/database/calculadora/py/draft.py
@@ -1,34 +1,40 @@
 class Calculadora:
-      def __init__(self, batteryMax:int):
-        self.batteryMax = batteryMax
-        self.battery = 0
-        self.display = 0.0
+    def __init__(self, batteryMax:int):
+       self.display : float = 0.0
+       self.battery : int = 0
+       self.batteryMax :int = batteryMax
       
-      def __str__ (self) -> str:
+      
+    def __str__ (self) -> str:
             return f"display = {self.display:.2f}, battery = {self.battery}"
 
-      def charge (self, increment:int):
-          if self.battery < self.batteryMax: 
-             self.battery += increment
-             if self.battery > self.batteryMax:# aqui verifica se a bateria passou do limite após o incremento
-              self.battery = self.batteryMax # se sim, aqui freia e diz que deve estar no limite. 
+    def charge (self, increment:int):
+        self.battery += increment
+        if self.battery >= self.batteryMax :
+            self.battery = self.batteryMax
+            
 
-      def sum (self, a:int , b:int):
-            if self.battery ==0:
-               print ("fail: bateria insuficiente")
-            else:
-               self.display = a + b
-               self.battery -= 1 
+    def sum (self, a:int , b:int):
+        if self.battery == 0:
+            print ("fail: bateria insuficiente")
+        else:
+            self.display = a + b 
+            self.battery -=1
 
-      def div (self, a :int , b:int):
-           if self.battery ==0:
-               print ("fail: bateria insuficiente")
-           elif b== 0 :
-               print ("fail: divisao por zero")  
-               self.battery -= 1
-           else:
-                 self.display = a / b
-                 self.battery -= 1  
+    def div (self, a :int , b:int):
+        if self.battery == 0:
+             print ("fail: bateria insuficiente")
+        elif  b == 0 :
+              print ("fail: divisao por zero")
+              self.battery -=1
+        else:
+              self.display = a / b
+              self.battery -=1
+
+               
+              
+    
+
 
 def main ():
     calculadora=None
--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,51 +1,56 @@
 class Carro:
-    def __init__(self, pass3 : int , km:int , gas:int):
-       self.pass3: int = 0
-       self.km: int = 0
-       self.passMax: int = 2
-       self.gas : int = 0
-       self.gasMax : int = 100
+    def __init__(self):
+        self.pass5: int = 0
+        self.km : int = 0
+        self.passMax : int = 2
+        self.gas : int = 0
+        self.gasMax : int = 100
 
     def __str__ (self) -> str:
-           return (f"pass: {self.pass3}, gas: {self.gas}, km: {self.km}")
+           return (f"pass: {self.pass5}, gas: {self.gas}, km: {self.km}")
        
-    def enter (self)-> None :
-        if self.pass3 < self.passMax:
-            self.pass3 += 1
-        else:
-            print("fail: limite de pessoas atingido")    
-           
+    def enter (self):
+        self.pass5 += 1
+        if self.pass5 > self.passMax:
+            print ("fail: limite de pessoas atingido")
+            self.pass5 = self.passMax
         
-    def leave(self) -> None:
-      if self.pass3 <= 0:
-        print("fail: nao ha ninguem no carro")
-        return
-      self.pass3 -= 1
+    def leave (self):
+        if self.pass5 == 0:
+            print("fail: nao ha ninguem no carro")
+        else:
+            self.pass5 -= 1
 
+            
          
-    def fuel (self,increment:int):     
+    def fuel (self, increment : int):
         self.gas += increment
         if self.gas > 100 :
-            self.gas = 100
+           self.gas = 100
+
 
     def drive (self,distance: int)-> None:
-         if self.pass3 == 0:
-            print ("fail: nao ha ninguem no carro")
-            return
+         if self.pass5 == 0:
+             print ("fail: nao ha ninguem no carro")
+             return
          if self.gas == 0:
-            print ("fail: tanque vazio")
-            return
+             print ("fail: tanque vazio")
+             return
          if distance<= self.gas :
              self.km += distance
              self.gas -= distance
+
+
          else:
              kmandado =self.gas
              self.km += kmandado
              self.gas=0
              print (f"fail: tanque vazio apos andar {kmandado} km")
 
+
+
 def main():
-    carro=Carro (" " , " " , " ") 
+    carro = Carro()
     while True: 
         line : str = input()     
         print ("$" + line)  
